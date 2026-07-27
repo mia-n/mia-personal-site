ba33781b881b83e7069fc97b79e1fb4adcc014c4
Mia North <mianorth@Mias-MBP.cutler.casa>
2026-07-27 12:36:16 -0400

    move about me image

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,15 +3,15 @@ import { siteConfig } from '../data/site'
 export default function About() {
   return (
     <article className="stack-lg prose">
+      <h1>About</h1>
+      <p>
+        I'm Mia. I've been a professional software engineer for a while, mostly working on large-scale maintainable platforms. I've done a lot of infra work too.
+      </p>
       <img
         src="/images/about-photo.jpg"
         alt="Mia wearing a headset in the cockpit of a small airplane"
         style={{ display: 'block', margin: '0 auto', maxWidth: '320px' }}
       />
-      <h1>About</h1>
-      <p>
-        I'm Mia. I've been a professional software engineer for a while, mostly working on large-scale maintainable platforms. I've done a lot of infra work too.
-      </p>
       <p>
         In my personal life I play roller derby, fly planes, and watch birds.
       </p>
